e7f6c4ba7423b67b6db6b2bcf99ef7aa3a36b392
Dmytro Podoprosvietov <mid8f0ssl@gmail.com>
2023-10-18 22:34:01 +0300

    config: update gatsby config to respect json transformations

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,21 @@ module.exports = {
     title: `Middle Infinity Fossil`,
     siteUrl: `https://mid8f0ssl.me`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-json",
+    "gatsby-transformer-sharp",
+   {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/logo.svg"
     }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
@@ -25,6 +34,49 @@ module.exports = {
       "path": "./src/pages/"
     },
     __key: "pages"
+  }, {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: "author",
+      path: "./src/data/author/",
+      ignore: ["**/\.*"],
+      fastHash: true,
+    },
+    __key: "author"
+  },{
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "meta",
+        path: "./src/data/meta/",
+        ignore: ["**/\.*"],
+        fastHash: true,
+      },
+      __key: "meta"
+    }, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "cv",
+        path: "./src/data/cv/",
+        ignore: ["**/\.*"],
+        fastHash: true,
+      },
+      __key: "cv"
+    }, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "projects",
+        path: "./src/data/projects/",
+        ignore: ["**/\.*"],
+        fastHash: true,
+      },
+      __key: "projects"
+    }, {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `blog`,
+      path: `${__dirname}/src/blog/`,
+    },
+    __key: 'blog'
   }, {
     resolve: `gatsby-plugin-google-gtag`,
     options: {
