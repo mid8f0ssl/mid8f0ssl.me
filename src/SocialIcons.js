6ac1e9600245770033ab20dae12cc068d8026742
Dmytro Podoprosvietov <mid8f0ssl@gmail.com>
2023-10-22 22:12:36 +0300

    feat(navigation): add email to social link

--- src/SocialIcons.js
+++ src/SocialIcons.js
@@ -4,9 +4,11 @@ import {
   BiLogoLinkedinSquare,
   BiLogoUpwork,
   BiLogoGithub,
+  BiLogoGmail,
 } from 'react-icons/bi'
 
 export default {
+  email: <BiLogoGmail className="h-6 w-6 m500:h-4 m500:w-4" />,
   github: <BiLogoGithub className="h-6 w-6 m500:h-4 m500:w-4" />,
   instagram: <BiLogoInstagramAlt className="h-6 w-6 m500:h-4 m500:w-4" />,
   linkedin: <BiLogoLinkedinSquare className="h-6 w-6 m500:h-4 m500:w-4" />,
